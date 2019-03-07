7723288e8ae1f59ee233df222ee54c65be6eb052
Harry <harry@lianghuangdeMBP.lan>
2019-03-07 20:05:12 +0800

    修改文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
     production: {
       user: 'root',
       host: '119.27.167.20',
-      ref: 'origin-github/master',
+      ref: 'origin/master',
       repo: 'git@github.com:HarryPoint/pm2-deploy.git',
       path: '/var/www/node-server/pm2-deploy',
       'post-deploy':
