59e29cfe62bf8ecc74bc255b6d8162e39ac7f4bd
Harry <harry@lianghuangdeMBP.lan>
2019-03-07 17:34:34 +0800

    添加配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pm2-deploy',
+      script: 'server/index.js',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '119.27.167.20',
+      ref: 'origin-github/master',
+      repo: 'git@github.com:HarryPoint/pm2-deploy.git',
+      path: '/var/www/node-server/pm2-deploy',
+      'post-deploy':
+        'yarn install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
