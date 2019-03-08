f55227ab18871451d82ee3cc094fa5b4614631b1
Harry <harry@lianghuangdeMBP.lan>
2019-03-08 15:10:22 +0800

    部署正式服

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -135,7 +135,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"isTest":true}
+      env: {"isTest":false}
     }
     // Only set once
     if (context.req) app.context.req = context.req
